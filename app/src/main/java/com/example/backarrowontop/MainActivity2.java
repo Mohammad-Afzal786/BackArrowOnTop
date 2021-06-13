db9469e96b15476ff6b52194fe436e922c2bb38b
Mohammad-Afzal <affutech1@gmail.com>
2021-06-13 11:45:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/backarrowontop/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.backarrowontop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
